17766c7dcfa3f415bd493a8b503735c43951eb05
kperez <karla@dayuse.com>
2024-02-25 18:49:38 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+import {inter} from "@/app/ui/fonts";
+import {poppins} from "@/app/ui/fonts";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Poppins, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const poppins = Poppins({weight: '400', subsets: ['latin']});
+export const lusitana = Lusitana({weight: ['400', '700'], subsets: ['latin']});
